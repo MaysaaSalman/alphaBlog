8af57d6874b7a919bb636f9ea9b0303d204c68b3
Maysa test <test@test.com>
2020-09-02 16:03:46 +0300

    Articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :articles
   root "pages#homePage"
-  get 'about', to : 'pages#homePage'
+  get 'about', to: 'pages#about'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
